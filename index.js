96477e7f410abc92f758a499d2843e5a3b7b7269
Brandon Cassidy <brandoncassidy11@yahoo.com>
2018-02-07 22:36:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,15 @@ function nowServing (katzDeliLine) {
 }
 
 function currentLine (line) {
-  var lineStr = '';
+  var lineStr = 'The line is currently: ';
   if (line.length === 0) {
     return 'The line is currently empty';
   }
   
   for (var i = 0; i < line.length; i++) {
-   lineStr += `The line is currently: ${i + 1}. ${line[i]}`
+   if (line.length === 1) {
+     lineStr += `${i + 1}. ${line[i]}`;
+   }
+   lineStr += `${i + 1}. ${line[i]},`
   }
 }
