7201cd8496b9caba8d9aa76095febbc926c43b6f
Brandon Cassidy <brandoncassidy11@yahoo.com>
2018-02-07 22:38:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine (line) {
   }
   
   for (var i = 0; i < line.length; i++) {
-   if (line.length === 1) {
+   if (line.length === 1 || i === line[line.length - 1]) {
      lineStr += `${i + 1}. ${line[i]}`;
    }
    lineStr += `${i + 1}. ${line[i]},`
