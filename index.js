e501e4abf65b288c8aeac7c2aa58fd891c2fd812
Brandon Cassidy <brandoncassidy11@yahoo.com>
2018-02-07 22:26:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing () {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!';
+  }
+  
+  return katzDeliLine.shift
+}
